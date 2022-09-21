71e7c634b0353ebb464240dc25d842b695e507c1
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 21:21:28 +0600

    pattern

---
+++ pattern 16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main ()
+{
+    int row,col,n;
+    printf("Entter the value of n=");
+    scanf("%d",&n);
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%c ",35);
+        }
+        printf("\n");
+    }
+    for(row=(n-1);row>=1;row--)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%c ",35);
+        }
+        printf("\n");
+    }
+}
+
---
+++ pattern 9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+ int row,col,n;
+ int i=1;
+ printf("Entter the value of n=");
+    scanf("%d",&n);
+    for(row=n;row>=1;row--)
+    {
+        for(col=1;col<=row;col++)
+        {
+            printf("%d ",col);
+        }
+        printf("\n");
+    }
+}
