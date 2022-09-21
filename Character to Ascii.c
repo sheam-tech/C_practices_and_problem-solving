6fcf72c9f17595d40f28154e8d09176115f9ad75
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 17:56:41 +0600

    basic

---
+++ Character to Ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char character;
+    printf("enter the character value=");
+    scanf("%c",&character);
+    printf("The charecter is=%d",character);
+    return 0;
+}
