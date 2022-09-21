2cfcf80f990374287af61784002f926a063ab9a5
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 18:37:35 +0600

    header

---
+++ test Math.h .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x;
+  scanf("%lf",&x);
+   // int y=abs(x);
+   // double y=sqrt(x);
+    double y;
+  scanf("%lf",&y);
+   // int y=abs(x);
+   // double y=sqrt(x);
+   double z=pow(x,y);
+    printf("%lf",z);
+
+    return 0;
+}
