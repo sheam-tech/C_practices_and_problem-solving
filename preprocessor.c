240c6bc5c9e2341e44a86a0a7258d19bab8eb5ae
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 19:26:16 +0600

    preprocessor

---
+++ preprocessor.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "info.h"
+#define pi 3.1416
+#define name "tarikul islam"
+int main()
+{  int p=2;
+    f
+    f
+    f
+    add(3,4);
+    printf("%f\n",pi);
+    printf("%s",name);
+}
