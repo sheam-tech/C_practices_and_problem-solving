88e25daf03d954edcac069ecf57dc3e0b52c446a
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 18:53:37 +0600

    sizeof

---
+++ sizeof.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int a;
+  long int y;
+  char b;
+  float c;
+  double d;
+  long double x;
+  printf("size of int=%d\n",sizeof(a));
+  printf("size of long int=%d\n",sizeof(y));
+  printf("size of char=%d\n",sizeof(b));
+  printf("size of float=%d\n",sizeof(c));
+  printf("size of long float=%d\n",sizeof(x));
+  printf("size of double=%d\n",sizeof(d));
+
+return 0;
+}
