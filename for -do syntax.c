4c0b55953b7574318933cb741c09a09e2db73381
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 18:08:49 +0600

    iteration

---
+++ for -do syntax.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+   // int i;
+  //  for(i=2;i<=10;i=i+2) //intialize;condition;counter_update
+   // {
+   // printf("%d.Sheam\n",i);
+  //  }
+  //int i=2;
+   // while(i<=10)
+//{
+ //   printf("%d.Sheam\n",i);
+ //   i=i+2;
+/*}*/
+int i=2;//intialize
+do{
+    printf("%d.Sheam\n",i);
+    i=i+2;//counter_update// age do er vitor kaj kore , then while er condition check kore
+}
+while(i<=10);//condition
+return 0;
+}
