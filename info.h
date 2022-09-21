2be30834018a53db45cb5ca9f82ab42d045c3ae4
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-09-21 18:12:01 +0600

    header

---
+++ info.h
@@ -0,0 +1,8 @@
+#define pi 3.1416
+#define name "tarikul islam"
+#define f printf("Hello\n");
+void add(float a, float b){
+ float x;
+ x=a+b;
+ printf("\n%.2f+%.2f=%.2f\n",a,b,x);
+}
---
+++ using created header file.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"info.h"//"" for the same directory header file, <> for the standard location of the compiler
+int main()
+{
+    f
+    f
+    f
+    printf("%f\n",pi);
+    printf("%s",name);
+    add(5,6);
+}
