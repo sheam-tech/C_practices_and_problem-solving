70581e53833066b4fc625efa3819d6348f68f263
Md Tarikul Islam Sheam <tarikulislam.sheam@gmail.com>
2022-10-06 03:04:45 +0600

    optimized

--- Basic_practices/Prime Checker.c
+++ Basic_practices/Prime Checker.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+int is_prime(int n)
+ {
+     int i, root;
+     if(n == 2) {
+         return 1;
+     }
+     if(n % 2 == 0) {
+         return 0;
+     }
+     root = sqrt(n);
+     for(i = 3; i <= root; i = i + 2) {
+         if(n % i == 0) {
+             return 0;
+         }
+     }
+   return 1;
+ }
+
 int main()
 {
 
+/*--------------------- inefficient Algo---------------
  int i,a;
  int s=1;
 while(1){
@@ -23,8 +42,23 @@ while(1){
 
     }
     }  }
-
- return 0;
+*/
+//--------------------- Better Algo ---------------------
+int n, m;
+     while(1) {
+         printf("Please enter a number (enter 0 to exit): ");
+         scanf("%d", &n);
+         if(n == 0) {
+             break;
+         }
+         if(1 == is_prime(n)) {
+             printf("%d is a prime number.\n", n);
+         }
+         else {
+             printf("%d is not a prime number.\n", n);
+         }
+     }
+     return 0;
 
 }
 
